579f3e848ad5cd963792f2f92abe5b024727aabd
Shashwat2998 <110040619+Shashwat2998@users.noreply.github.com>
2023-03-14 16:59:34 +0530

    Add files via upload

---
+++ Django_Assg/Customer_Management_Service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CustomerManagementServiceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Customer_Management_Service"
